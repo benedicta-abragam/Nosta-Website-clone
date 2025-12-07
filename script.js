ca22272e58b0c7a2ff8f236d37d720e760e1af28
benedicta-abragam <benedictaabraham079@gmail.com>
2025-12-08 01:35:31 +0530

    changes updated

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+// topup
+let topup = document.querySelector(".topup")
+let cross = document.getElementById("cross")
+cross.addEventListener("click", function(){
+    topup.style.display = "none"
+})
+//  side navbar
+  let openbtn = document.getElementById("side-navbar")
+  let closebtn = document.getElementById("side-navbar-close")
+  let sidenav = document.querySelector(".side-menu")
+  openbtn.addEventListener("click", 
+    function(){
+        sidenav.style.marginLeft = "0"
+   })
+   closebtn.addEventListener("click", function(){
+        sidenav.style.marginLeft = "-60%"
 
+   })
+
+// slider
 let leftbtn = document.getElementById("slider-left")
 let rightbtn = document.getElementById("slider-right")
 let imageslider = document.querySelector(".slider__img")
@@ -28,3 +47,4 @@ leftbtn.addEventListener("click", function(){
      
     imageslider.style.marginLeft = "-" + slidermargin + "vw";
 })
+
