f06b934aa3d82978967b1e708d57046e84b8252b
benedicta-abragam <benedictaabraham079@gmail.com>
2025-12-07 22:10:28 +0530

    changes in javascript

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-let topup = document.querySelector(".topup")
-let cross = document.getElementById("cross")
-cross.addEventListener("click", function(){
-    topup.style.display ="none"
+
+let leftbtn = document.getElementById("slider-left")
+let rightbtn = document.getElementById("slider-right")
+let imageslider = document.querySelector(".slider__img")
+let slidermargin = 0
+
+rightbtn.addEventListener("click", function(){
+    slidermargin = slidermargin + 100
+
+    if(slidermargin > 200)
+    {
+        slidermargin = 0
+        imageslider.style.marginleft= 0;
+    }
+    else{
+        imageslider.style.marginLeft = "-" + slidermargin + "vw";
+    }
+})
+
+leftbtn.addEventListener("click", function(){
+    if(slidermargin == 0)
+    {
+        slidermargin = 200;
+    }
+    else{
+        slidermargin = slidermargin -100;
+    }
+     
+    imageslider.style.marginLeft = "-" + slidermargin + "vw";
 })
